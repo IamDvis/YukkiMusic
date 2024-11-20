a3c4500dd6fd480063159be487f8cd5f2dc05d19
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-11-20 22:13:48 +0530

    Update Telegram.py
    Signed-off-by: Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>

--- YukkiMusic/platforms/Telegram.py
+++ YukkiMusic/platforms/Telegram.py
@@ -22,13 +22,8 @@ from config import lyrical
 from ..utils.formatters import convert_bytes, get_readable_time, seconds_to_min
 
 from telethon import Button, events
-from telethon.tl.types import (
-    DocumentAttributeFilename,
-    DocumentAttributeAudio,
-    DocumentAttributeVideo,
-    MessageMediaDocument,
-)
-
+from telethon.tl.custom.message import Message
+from telethon.tl.custom.file import File
 
 downloader = {}
 
@@ -60,52 +55,25 @@ class TeleAPI:
         return link
 
     async def get_filename(
-        self, event: events.NewMessage, audio: Union[bool, str] = None
+        self, file: File, audio: Union[bool, str] = None
     ):
-        try:
-            if event.message.media and event.message.media.document:
-                file_name = None
-                for attribute in event.message.media.document.attributes:
-                    if isinstance(attribute, DocumentAttributeFilename):
-                        file_name = attribute.file_name
-                        break
-
-                if file_name is None:
-                    file_name = (
-                        "Telegram audio file" if audio else "Telegram video file"
-                    )
-            else:
-                file_name = "Telegram audio file" if audio else "Telegram video file"
-
-        except Exception:
-            file_name = "Telegram audio file" if audio else "Telegram video file"
-
+            file_name = file.name or "Telegram audio file" if audio else "Telegram video file"
         return file_name
 
-    async def get_duration(self, event: events.NewMessage):
+    async def get_duration(self, file: File): -> str:
         try:
-            duration = None
-            if event.message.media and event.message.media.document:
-                for attribute in event.message.media.document.attributes:
-                    if isinstance(
-                        attribute, (DocumentAttributeAudio, DocumentAttributeVideo)
-                    ):
-                        duration = attribute.duration
-                        break
+            duration = file.duration
             dur = seconds_to_min(duration) if duration is not None else "Unknown"
         except Exception:
             dur = "Unknown"
         return dur
 
-    async def get_filepath(self, event: events.NewMessage.Event) -> str:
-        file_name = ""
-
-        if event.media:
-            file_id = event.file.id
-            file_ext = event.file.ext or ""
-            file_name = f"{file_id}{file_ext}"
-            downloads_dir = os.path.realpath("downloads")
-            file_name = os.path.join(downloads_dir, file_name)
+    async def get_filepath(self, file: File) -> str:
+        file_id = file.id
+        file_ext = file.ext or ""
+        file_name = f"{file_id}{file_ext}"
+        downloads_dir = os.path.realpath("downloads")
+        file_name = os.path.join(downloads_dir, file_name)
 
         return file_name
 
@@ -151,7 +119,7 @@ class TeleAPI:
                 current_time = time.time()
                 start_time = speed_counter.get(event.id)
                 check_time = current_time - start_time
-                upl = [Button.inline("🚦 ᴄᴀɴᴄᴇʟ ᴅᴏᴡɴʟᴏᴀᴅɪɴɢ", "stop_downloading")]
+                upl = [Button.inline("🚦 Cancel Downloading", "stop_downloading")]
                 if datetime.now() > left_time.get(event.id):
                     percentage = current * 100 / total
                     speed = current / check_time
@@ -172,8 +140,8 @@ class TeleAPI:
 **Speed:** {speed}/s
 **Elapsed Time:** {eta}"""
                     try:
-                        await event.client.edit_message(
-                            event.chat_id, mystic, text, buttons=upl
+                        await mystic.edit(
+                            message=text, buttons=upl
                         )
                     except:
                         pass
@@ -183,19 +151,17 @@ class TeleAPI:
             left_time[event.id] = datetime.now()
 
             try:
-                await event.client.download_media(
-                    event.reply_to_msg_id,  # Assuming direct reply-to context
+                await app.download_media(
+                    event.reply_to_msg_id,
                     file=fname,
                     progress_callback=progress,
                 )
-                await event.client.edit_message(
-                    event.chat_id,
-                    mystic,
+                await mystic.edit(
                     "Successfully downloaded... Processing file now",
                 )
                 downloader.pop(event.id, None)
             except:
-                await event.client.edit_message(event.chat_id, mystic, _["tg_2"])
+                await mystic.edit(_["tg_2"])
 
             if len(downloader) > 10:
                 timers = [downloader[x] for x in downloader]
@@ -204,8 +170,7 @@ class TeleAPI:
                     eta = get_readable_time(low)
                 except:
                     eta = "Unknown"
-                await event.client.edit_message(
-                    event.chat_id, mystic, _["tg_1"].format(eta)
+                await mystic.edit(_["tg_1"].format(eta)
                 )
                 return False
 
